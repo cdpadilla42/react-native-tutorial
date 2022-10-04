6c1322db05072a422dd9d6afba3a4e72e1dc10a5
Chris Padilla <cdpadilla42@gmail.com>
2022-10-04 11:15:32 -0500

    Test text component

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Image } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log('pressed');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Open up App.js to start working on your app! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Est nobis repudiandae nihil dolore,
+        perspiciatis quaerat quasi doloremque voluptatibus sit, laudantium eius
+        dignissimos corrupti laboriosam neque corporis magni asperiores deserunt
+        non! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
+        obcaecati recusandae veniam dolorem, fugit molestiae quis fugiat
+        quisquam similique aliquam doloremque voluptates autem. Totam dicta odit
+        sed inventore alias recusandae!
+      </Text>
+      <Image
+        source={{
+          uri: 'https://picsum.photos/200/300',
+          width: 200,
+          height: 300,
+        }}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
